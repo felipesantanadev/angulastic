ea09410d1854ccc9dfa508768433c2a527c35795
Felipe Santana <felipe.ssilva@unitau.com.br>
2018-06-26 12:00:24 -0300

    Docker compose for Elasticsearch, Logstash and Kibana

---
+++ angulastic-app/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import {ClusterComponent} from '../app/cluster/cluster.component';
+
+const routes: Routes = [
+  { path: '', component: ClusterComponent }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  declarations: [],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- angulastic-app/src/app/app.module.ts
+++ angulastic-app/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from '../app/app-routing.module';
+import { ClusterComponent } from './cluster/cluster.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClusterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
